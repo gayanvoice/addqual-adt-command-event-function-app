25c9e7ef4a9b09d91dd30a7538078941e5237783
gayan <gayan.kuruppu@live.com>
2023-09-22 12:59:33 +0100

    feat: ur gripper

--- AddQualADTCommandEventFunctionApp/CommandEventFunction.cs
+++ AddQualADTCommandEventFunctionApp/CommandEventFunction.cs
@@ -5,15 +5,55 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.EventGrid;
 using Microsoft.Extensions.Logging;
 using Azure.Messaging.EventGrid;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using Azure.Identity;
+using Azure.DigitalTwins.Core;
 
 namespace AddQualADTCommandEventFunctionApp
 {
     public static class CommandEventFunction
     {
+        private static readonly string ADT_SERVICE_URL = Environment.GetEnvironmentVariable("ADT_SERVICE_URL");
         [FunctionName("CommandEventFunction")]
         public static void Run([EventGridTrigger]EventGridEvent eventGridEvent, ILogger log)
         {
-            log.LogInformation(eventGridEvent.Data.ToString());
+            DefaultAzureCredential defaultAzureCredential = new DefaultAzureCredential();
+            DigitalTwinsClient digitalTwinsClient = new DigitalTwinsClient(endpoint: new Uri(ADT_SERVICE_URL), credential: defaultAzureCredential);
+
+            if (eventGridEvent != null && eventGridEvent.Data != null)
+            {
+                JObject jObject = JsonConvert.DeserializeObject<JObject>(eventGridEvent.Data.ToString());
+                log.LogInformation(jObject["dataschema"].ToString());
+
+                //if (jObject["dataschema"].ToString().Equals("dtmi:com:AddQual:Factory:ScanBox:Cobot:URCobot;1"))
+                //{
+                //    URCobotModel urCobotModel = JsonConvert.DeserializeObject<URCobotModel>(eventGridEvent.Data.ToString());
+                //    Azure.JsonPatchDocument azureJsonPatchDocument = new Azure.JsonPatchDocument();
+                //    JointPositionModel jointPositionModel = JointPositionModel.GetDegrees(urCobotModel);
+                //    azureJsonPatchDocument.AppendAdd("/IsPaused", true);
+                //    azureJsonPatchDocument.AppendAdd("/IsSafetyPopupClosed", true);
+                //    azureJsonPatchDocument.AppendAdd("/IsProtectiveStopUnlocked", true);
+                //    azureJsonPatchDocument.AppendAdd("/IsPowerOn", true);
+                //    azureJsonPatchDocument.AppendAdd("/IsFreeDriveModeEnabled", true);
+                //    azureJsonPatchDocument.AppendAdd("/IsTeachModeEnabled", true);
+                //    azureJsonPatchDocument.AppendAdd("/JointPosition", jointPositionModel);
+                //    azureJsonPatchDocument.AppendAdd("/IsInvoked", false);
+                //    await digitalTwinsClient.UpdateDigitalTwinAsync("URCobot", azureJsonPatchDocument);
+                //}
+                //else if (jObject["dataschema"].ToString().Equals("dtmi:com:AddQual:Factory:ScanBox:Cobot:URGripper;1"))
+                //{
+                //    URGripperModel urGripperModel = JsonConvert.DeserializeObject<URGripperModel>(eventGridEvent.Data.ToString());
+                //    Azure.JsonPatchDocument azureJsonPatchDocument = new Azure.JsonPatchDocument();
+                //    azureJsonPatchDocument.AppendAdd("/IsActive", urGripperModel.data.ACT);
+                //    if (urGripperModel.data.ACT == 1) azureJsonPatchDocument.AppendAdd("/IsActive", true);
+                //    else azureJsonPatchDocument.AppendAdd("/IsActive", false);
+                //    if (urGripperModel.data.POS < 10) azureJsonPatchDocument.AppendAdd("/IsOpen", true);
+                //    else azureJsonPatchDocument.AppendAdd("/IsOpen", false);
+                //    azureJsonPatchDocument.AppendAdd("/IsInvoked", false);
+                //    await digitalTwinsClient.UpdateDigitalTwinAsync("URGripper", azureJsonPatchDocument);
+                //}
+            }
         }
     }
 }
