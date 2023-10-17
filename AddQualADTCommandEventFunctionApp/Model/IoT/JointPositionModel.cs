6cb9c297c14f1d210de238eba918f8c63883b2b9
gayanvoice <gayan.kuruppu@live.com>
2023-10-17 01:42:10 +0100

    fix: model error

--- AddQualADTCommandEventFunctionApp/Model/IoT/JointPositionModel.cs
+++ AddQualADTCommandEventFunctionApp/Model/IoT/JointPositionModel.cs
@@ -10,16 +10,16 @@ namespace AddQualADTCommandEventFunctionApp.Model.IoT
         public double _wrist1 { get; set; }
         public double _wrist2 { get; set; }
         public double _wrist3 { get; set; }
-        public static JointPositionModel Get(DigitalTwins.JointPositionModel digitalTwinsJointPositionModel)
-        {
-            JointPositionModel iotJointPositionModel = new JointPositionModel();
-            iotJointPositionModel._base = digitalTwinsJointPositionModel.Base;
-            iotJointPositionModel._shoulder = digitalTwinsJointPositionModel.Shoulder;
-            iotJointPositionModel._elbow = digitalTwinsJointPositionModel.Elbow;
-            iotJointPositionModel._wrist1 = digitalTwinsJointPositionModel.Wrist1;
-            iotJointPositionModel._wrist2 = digitalTwinsJointPositionModel.Wrist2;
-            iotJointPositionModel._wrist3 = digitalTwinsJointPositionModel.Wrist3;
-            return iotJointPositionModel;
-        }
+        //public static JointPositionModel Get(DigitalTwins.JointPositionModel digitalTwinsJointPositionModel)
+        //{
+        //    JointPositionModel iotJointPositionModel = new JointPositionModel();
+        //    iotJointPositionModel._base = digitalTwinsJointPositionModel.Base;
+        //    iotJointPositionModel._shoulder = digitalTwinsJointPositionModel.Shoulder;
+        //    iotJointPositionModel._elbow = digitalTwinsJointPositionModel.Elbow;
+        //    iotJointPositionModel._wrist1 = digitalTwinsJointPositionModel.Wrist1;
+        //    iotJointPositionModel._wrist2 = digitalTwinsJointPositionModel.Wrist2;
+        //    iotJointPositionModel._wrist3 = digitalTwinsJointPositionModel.Wrist3;
+        //    return iotJointPositionModel;
+        //}
     }
 }
