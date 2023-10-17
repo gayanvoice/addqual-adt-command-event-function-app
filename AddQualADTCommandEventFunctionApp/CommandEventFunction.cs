dcdc7110089eaf86f9e593fa342ab04ac2907f16
gayanvoice <gayan.kuruppu@live.com>
2023-10-17 01:17:15 +0100

    refactor: ur cobot basic digital twins

--- AddQualADTCommandEventFunctionApp/CommandEventFunction.cs
+++ AddQualADTCommandEventFunctionApp/CommandEventFunction.cs
@@ -32,47 +32,29 @@ namespace AddQualADTCommandEventFunctionApp
                 if (rootObjectModel.Data.ModelId.Equals("dtmi:com:AddQual:Factory:ScanBox:Cobot:URCobot;1"))
                 {
                     BasicDigitalTwin urCobotBasicDigitalTwin = await GetBasicDigitalTwinAsync(twinId: "URCobot", digitalTwinsClient: digitalTwinsClient);
-                    URCobotTwinModel urCobotTwinModel = URCobotTwinModel.GetFromBasicDigitalTwin(urCobotBasicDigitalTwin);
-                    if (urCobotTwinModel.IsInvoked)
-                    {
-                        Model.IoT.JointPositionModel jointPositionModel = Model.IoT.JointPositionModel.Get(urCobotTwinModel.ActualQJointPosition);
-                        List<Model.IoT.JointPositionModel> digitalTwinsJointPositionModelList = new List<Model.IoT.JointPositionModel>
-                        {
-                            jointPositionModel
-                        };
-                        MoveJCommandModel moveJCommandModel = MoveJCommandModel.Get(digitalTwinsJointPositionModelList: digitalTwinsJointPositionModelList);
-                        string moveJCommandPayload = JsonConvert.SerializeObject(moveJCommandModel);
-                        ServiceClient serviceClient = ServiceClient.CreateFromConnectionString(IOT_HUB_SERVICE_URL);
-                        CloudToDeviceMethod cloudToDeviceMethod = new CloudToDeviceMethod("MoveJCommand");
-                        cloudToDeviceMethod.SetPayloadJson(moveJCommandPayload);
-                        CloudToDeviceMethodResult cloudToDeviceMethodResult = await serviceClient.InvokeDeviceMethodAsync("URCobot", cloudToDeviceMethod);
-                        log.LogInformation(JsonConvert.SerializeObject(cloudToDeviceMethodResult));
-                        log.LogInformation("UR COBOT EXECUTED" + JsonConvert.SerializeObject(moveJCommandModel));
-                    }
-                    else
-                    {
-                        log.LogInformation("UR COBOT NOT EXECUTED");
-                    }
-                }
-                else if (rootObjectModel.Data.ModelId.Equals("dtmi:com:AddQual:Factory:ScanBox:Cobot:URGripper;1"))
-                {
-                    BasicDigitalTwin urGripperBasicDigitalTwin = await GetBasicDigitalTwinAsync(twinId: "URGripper", digitalTwinsClient: digitalTwinsClient);
-                    URGripperTwinModel urGripperTwinModel = URGripperTwinModel.GetFromBasicDigitalTwin(urGripperBasicDigitalTwin);
-                    if (urGripperTwinModel.IsInvoked)
-                    {
-                        ServiceClient serviceClient = ServiceClient.CreateFromConnectionString(IOT_HUB_SERVICE_URL);
-                        CloudToDeviceMethod cloudToDeviceMethod;
-                        if (urGripperTwinModel.IsOpen) cloudToDeviceMethod = new CloudToDeviceMethod("OpenGripperCommand");
-                        else cloudToDeviceMethod = new CloudToDeviceMethod("CloseGripperCommand");
-                        cloudToDeviceMethod.ResponseTimeout = TimeSpan.FromSeconds(10);
-                        CloudToDeviceMethodResult cloudToDeviceMethodResult = await serviceClient.InvokeDeviceMethodAsync("URGripper", cloudToDeviceMethod);
-                        log.LogInformation(JsonConvert.SerializeObject(cloudToDeviceMethodResult));
-                        log.LogInformation("UR GRIPPER EXECUTED");
-                    }
-                    else
-                    {
-                        log.LogInformation("UR GRIPPER NOT EXECUTED");
-                    }
+                    log.LogInformation(JsonConvert.SerializeObject(urCobotBasicDigitalTwin));
+
+                    //URCobotTwinModel urCobotTwinModel = URCobotTwinModel.GetFromBasicDigitalTwin(urCobotBasicDigitalTwin);
+                    //if (urCobotTwinModel.IsInvoked)
+                    //{
+                    //    Model.IoT.JointPositionModel jointPositionModel = Model.IoT.JointPositionModel.Get(urCobotTwinModel.ActualQJointPosition);
+                    //    List<Model.IoT.JointPositionModel> digitalTwinsJointPositionModelList = new List<Model.IoT.JointPositionModel>
+                    //    {
+                    //        jointPositionModel
+                    //    };
+                    //    MoveJCommandModel moveJCommandModel = MoveJCommandModel.Get(digitalTwinsJointPositionModelList: digitalTwinsJointPositionModelList);
+                    //    string moveJCommandPayload = JsonConvert.SerializeObject(moveJCommandModel);
+                    //    ServiceClient serviceClient = ServiceClient.CreateFromConnectionString(IOT_HUB_SERVICE_URL);
+                    //    CloudToDeviceMethod cloudToDeviceMethod = new CloudToDeviceMethod("MoveJCommand");
+                    //    cloudToDeviceMethod.SetPayloadJson(moveJCommandPayload);
+                    //    CloudToDeviceMethodResult cloudToDeviceMethodResult = await serviceClient.InvokeDeviceMethodAsync("URCobot", cloudToDeviceMethod);
+                    //    log.LogInformation(JsonConvert.SerializeObject(cloudToDeviceMethodResult));
+                    //    log.LogInformation("UR COBOT EXECUTED" + JsonConvert.SerializeObject(moveJCommandModel));
+                    //}
+                    //else
+                    //{
+                    //    log.LogInformation("UR COBOT NOT EXECUTED");
+                    //}
                 }
                 else
                 {
